e000596cb5bf9d19148b2b1f013bc53a7d823efd
Pankaj Negi <pnegi.dun@gmail.com>
2024-08-28 23:25:09 +0530

    refactor: :sparkles: added custom image picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,10 +1,19 @@
+'use client'
+import { useRef } from 'react';
 import classes from './image-picker.module.css';
 
-export default function ImagePicker({label, name}){
+export default function ImagePicker({ label, name }) {
+    const imageInput = useRef()
+    function handlePickClick() {
+        imageInput.current.click();
+    }
     return <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
-            <input type='file' id={name} accept='image/png, image/jpec' name={name}></input>
+            <input ref={imageInput} className={classes.input} type='file' id={name} accept='image/png, image/jpec' name={name}></input>
+            <button  onClick={handlePickClick} className={classes.button} type='button'>
+                Pick an Image
+            </button>
         </div>
     </div>
 }
